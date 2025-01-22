b8cad76cb419ef4b6a97f4360d52b2d67d4939c5
Colin Murphy <66136651+Colinster327@users.noreply.github.com>
2025-01-21 23:31:32 -0500

    fix: add debug env var (#4)

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 # SECURITY WARNING: update this when you have the production host
 ALLOWED_HOSTS = ['*']
