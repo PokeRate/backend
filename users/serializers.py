fd56a0a5c7a77c809dde5a98df1df18403bef368
Colin Murphy <66136651+Colinster327@users.noreply.github.com>
2025-02-03 16:12:22 -0500

    fix: user serializer permission issues (#11)

--- users/serializers.py
+++ users/serializers.py
@@ -14,6 +14,14 @@ class UserListSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     comments = CommentsListSerializerNoUser(many=True, read_only=True)
+    is_staff = serializers.BooleanField(read_only=True)
+    is_superuser = serializers.BooleanField(read_only=True)
+    is_active = serializers.BooleanField(read_only=True)
+    date_joined = serializers.DateTimeField(read_only=True)
+    last_login = serializers.DateTimeField(read_only=True)
+    groups = serializers.StringRelatedField(many=True, read_only=True)
+    user_permissions = serializers.StringRelatedField(
+        many=True, read_only=True)
 
     class Meta:
         model = User
